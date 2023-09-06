fbffb1ea9e444e974ef240a1ef19a65c696e8f82
SangNyungLee <lsy6747@gmail.com>
2023-09-06 21:00:09 +0900

    sangNyung

--- controller/MA_Cmain.js
+++ controller/MA_Cmain.js
@@ -1,284 +1,271 @@
-const { User } = require('../models/index');
-const bcrypt = require('bcrypt');
-const axios = require('axios');
-
+const { User } = require("../models/index");
+const bcrypt = require("bcrypt");
+const axios = require("axios");
 
 //카카오 rest api활용
 const kakao = {
-    clientID: process.env.KAKAO_ID,
-    redirectUri: 'http://localhost:8000/auth/kakao/callback'
-}
-
-
+  clientID: process.env.KAKAO_ID,
+  redirectUri: "http://localhost:8000/auth/kakao/callback",
+};
 
 /////////////////////GET///////////////////////
-exports.main = async (req, res)=>{
-    if(req.session.isLogined){
-        const data = await User.findOne({where: {id: req.session.userId}})
-        // console.log('프로필', data)
-        res.render('MA_profile', {data})
-    }else{
-        res.render('MA_main');
-    }
-}
+exports.main = async (req, res) => {
+  if (req.session.isLogined) {
+    const data = await User.findOne({ where: { id: req.session.userId } });
+    // console.log('프로필', data)
+    res.render("MA_profile", { data });
+  } else {
+    res.render("MA_main");
+  }
+};
 //회원가입
-exports.join = (req ,res)=>{
-    res.render('MA_join');
-}
+exports.join = (req, res) => {
+  res.render("MA_join");
+};
 //로그인
-exports.login = (req, res)=>{
-    if(req.session.isLogined){
-
-        res.render('MA_profile')
-    }else{
-        res.render('MA_login')
-    }
-}
+exports.login = (req, res) => {
+  if (req.session.isLogined) {
+    res.render("MA_profile");
+  } else {
+    res.render("MA_login");
+  }
+};
 //카카오 로그인 요청
-exports.kakao_get = (req, res)=>{
-    res.redirect(`https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile_nickname,account_email`)
-}
+exports.kakao_get = (req, res) => {
+  res.redirect(
+    `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile_nickname,account_email`
+  );
+};
 //카카오 로그인 응답받기
-exports.kakao_callback = async(req, res)=>{
-    try{//access토큰을 받기 위한 코드
-        token = await axios({
-            method: 'POST',
-            url: 'https://kauth.kakao.com/oauth/token',
-            headers:{
-                'content-type':'application/x-www-form-urlencoded'
-            },
-            data:{
-                grant_type: 'authorization_code',//특정 스트링
-                client_id:kakao.clientID, 
-                redirectUri:kakao.redirectUri,
-                code:req.query.code,
-            }
-        })
-    }catch(err){
-        res.json(err.data);
+exports.kakao_callback = async (req, res) => {
+  try {
+    //access토큰을 받기 위한 코드
+    token = await axios({
+      method: "POST",
+      url: "https://kauth.kakao.com/oauth/token",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        grant_type: "authorization_code", //특정 스트링
+        client_id: kakao.clientID,
+        redirectUri: kakao.redirectUri,
+        code: req.query.code,
+      },
+    });
+  } catch (err) {
+    res.json(err.data);
+  }
+  // console.log('token.data: ',token)
+  //access토큰을 받아서 사용자 정보를 알기 위해 쓰는 코드
+  let user;
+  try {
+    // console.log('token:',token);//access정보를 가지고 또 요청해야 정보를 가져올 수 있음.
+    user = await axios({
+      method: "get",
+      url: "https://kapi.kakao.com/v2/user/me",
+      headers: {
+        Authorization: `Bearer ${token.data.access_token}`,
+      },
+    });
+  } catch (e) {
+    res.json(e.data);
+  }
+  console.log("user: ", user.data);
+  try {
+    let nickname = user.data.properties.nickname;
+    let email = user.data.kakao_account.email;
+    const existCheck = await User.findOne({ where: { email } });
+    if (existCheck) {
+      //이메일 데이터가 존재하면
+      req.session.isLogined = true; // 세션생성,
+      req.session.userId = existCheck.id;
+      req.session.userName = existCheck.name;
+      //프로필 페이지로 연결
+      res.redirect("/profile");
+    } else {
+      //이메일이 없으면 db생성
+      await User.create({ email, name: nickname, snsId: "kakao" });
+      const user2 = await User.findOne({ where: { email } });
+      req.session.isLogined = true; //db생성후 세션생성
+      req.session.userId = user2.id;
+      req.session.userName = user2.name;
+      console.log(req.session);
+      //프로필 페이지로 연결
+      res.redirect("/profile");
     }
-    // console.log('token.data: ',token)
-    //access토큰을 받아서 사용자 정보를 알기 위해 쓰는 코드
-        let user;
-        try{
-            // console.log('token:',token);//access정보를 가지고 또 요청해야 정보를 가져올 수 있음.
-            user = await axios({
-                method:'get',
-                url:'https://kapi.kakao.com/v2/user/me',
-                headers:{
-                    Authorization: `Bearer ${token.data.access_token}`
-                }
-            })
-        }catch(e){
-            res.json(e.data);
-        }
-        console.log('user: ',user.data);
-        try{
-            let nickname = user.data.properties.nickname;
-            let email = user.data.kakao_account.email;
-            const existCheck = await User.findOne({where: {email}})
-            if(existCheck){ //이메일 데이터가 존재하면
-                req.session.isLogined = true; // 세션생성,
-                req.session.userId = existCheck.id;
-                req.session.userName = existCheck.name;
-                //프로필 페이지로 연결                 
-                res.redirect('/profile')
-            }else{ //이메일이 없으면 db생성
-                await User.create({email, name: nickname, snsId:'kakao'})
-                const user2 = await User.findOne({where: {email}})
-                req.session.isLogined = true; //db생성후 세션생성
-                req.session.userId = user2.id;
-                req.session.userName = user2.name;
-                console.log(req.session)
-                //프로필 페이지로 연결                 
-                res.redirect('/profile')
-                
-            }
-            
-        }catch(error){
-            console.log('카카오 회원가입 error',error)
-            res.status(500).json({result:false})
-        }
-
-}
+  } catch (error) {
+    console.log("카카오 회원가입 error", error);
+    res.status(500).json({ result: false });
+  }
+};
 
 //구글 rest api활용
 const google = {
-    clientID: process.env.GOOGLE_ID,
-    client_secret: process.env.CLIENT_SECRET_GOOGLE,
-    redirectUri: 'http://localhost:8000/auth/google/callback'
-}
+  clientID: process.env.GOOGLE_ID,
+  client_secret: process.env.CLIENT_SECRET_GOOGLE,
+  redirectUri: "http://localhost:8000/auth/google/callback",
+};
 
 //구글 로그인 요청
-exports.google_get = (req, res)=>{
-    res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${google.clientID}&redirect_uri=${google.redirectUri}&response_type=code&scope=email profile`)
-    console.log('로그인 요청 완료')
-}
-exports.google_callback= async (req,res)=>{
-    const {code} = req.query;
-    console.log('콜백왔다')
-    // console.log(`code: ${code}`);
+exports.google_get = (req, res) => {
+  res.redirect(
+    `https://accounts.google.com/o/oauth2/v2/auth?client_id=${google.clientID}&redirect_uri=${google.redirectUri}&response_type=code&scope=email profile`
+  );
+  console.log("로그인 요청 완료");
+};
+exports.google_callback = async (req, res) => {
+  const { code } = req.query;
+  console.log("콜백왔다");
+  // console.log(`code: ${code}`);
 
-
-    // access_token, refresh_token 등의 구글 토큰 정보 가져오기
-    const res1 = await axios({
-        method: "POST",
-        url: "https://oauth2.googleapis.com/token", 
-        // x-www-form-urlencoded(body)
-        data:{
-            code,
-            client_id: google.clientID,
-            client_secret: google.client_secret,
-            redirect_uri: google.redirectUri,
-            grant_type: 'authorization_code',
-        }
-    });
-    console.log('정보요청했다')
-    const user = await axios({
-        method:"get",
-        url:'https://www.googleapis.com/oauth2/v2/userinfo',
-        headers: {
-            Authorization: `Bearer ${res1.data.access_token}`
-        }
-    })
-    console.log(user.data);
-    console.log('왔다.')
-    try{
-        let name = user.data.name;
-        let email = user.data.email;
-        const existCheck = await User.findOne({where: {email}})
-        if(existCheck){ //이메일 데이터가 존재하면
-            req.session.isLogined = true; // 세션생성,
-            req.session.userId = existCheck.id;
-            req.session.userName = existCheck.name;
-            //프로필 페이지로 연결                 
-            res.redirect('/profile')
-        }else{ //이메일이 없으면 db생성
-            await User.create({email, name, snsId:'google'})
-            const user2 = await User.findOne({where: {email}})
-            req.session.isLogined = true; // 세션생성,
-            req.session.userId = user2.id;
-            req.session.userName = user2.name;
-            //프로필 페이지로 연결                 
-            res.redirect('/profile')
-        }
-        
-    }catch(error){
-        console.log('구글 회원가입 error',error)
-        res.status(500).json({result:false})
+  // access_token, refresh_token 등의 구글 토큰 정보 가져오기
+  const res1 = await axios({
+    method: "POST",
+    url: "https://oauth2.googleapis.com/token",
+    // x-www-form-urlencoded(body)
+    data: {
+      code,
+      client_id: google.clientID,
+      client_secret: google.client_secret,
+      redirect_uri: google.redirectUri,
+      grant_type: "authorization_code",
+    },
+  });
+  console.log("정보요청했다");
+  const user = await axios({
+    method: "get",
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    headers: {
+      Authorization: `Bearer ${res1.data.access_token}`,
+    },
+  });
+  console.log(user.data);
+  console.log("왔다.");
+  try {
+    let name = user.data.name;
+    let email = user.data.email;
+    const existCheck = await User.findOne({ where: { email } });
+    if (existCheck) {
+      //이메일 데이터가 존재하면
+      req.session.isLogined = true; // 세션생성,
+      req.session.userId = existCheck.id;
+      req.session.userName = existCheck.name;
+      //프로필 페이지로 연결
+      res.redirect("/profile");
+    } else {
+      //이메일이 없으면 db생성
+      await User.create({ email, name, snsId: "google" });
+      const user2 = await User.findOne({ where: { email } });
+      req.session.isLogined = true; // 세션생성,
+      req.session.userId = user2.id;
+      req.session.userName = user2.name;
+      //프로필 페이지로 연결
+      res.redirect("/profile");
     }
-}
+  } catch (error) {
+    console.log("구글 회원가입 error", error);
+    res.status(500).json({ result: false });
+  }
+};
 
 //프로필 페이지
-exports.profile = async (req,res)=>{
-    console.log('프로필 페이지',req.session.userName)
-    if(req.session.isLogined){ 
-        //세션이 있는경우 
-        const data = await User.findOne({where: {id: req.session.userId}})
-        // console.log('프로필', data)
-        res.render('MA_profile', {data})
-    }else {
-        res.render('MA_login');
-    }
-    
-}
+exports.profile = async (req, res) => {
+  console.log("프로필 페이지", req.session.userName);
+  if (req.session.isLogined) {
+    //세션이 있는경우
+    const data = await User.findOne({ where: { id: req.session.userId } });
+    // console.log('프로필', data)
+    res.render("MA_profile", { data });
+  } else {
+    res.render("MA_login");
+  }
+};
 
 //로그아웃
-exports.logout = (req,res)=>{
-    console.log(req.session.isLogined)
-    req.session.destroy()
-    console.log(req.session)
-    res.redirect('/?msg=logoutsuccess');
-}
-
-
-
+exports.logout = (req, res) => {
+  console.log(req.session.isLogined);
+  req.session.destroy();
+  console.log(req.session);
+  res.redirect("/?msg=logoutsuccess");
+};
 
 /////////////////////////POST///////////////////
 
-//회원가입 
-exports.post_join = async (req,res)=>{
-    console.log(req.body)
-    try{
-        const {email, name, phone, pw} = req.body;
-        console.log(req.body)
-        const existCheck = await User.findOne({where: {email}})
-        if(existCheck){ //이메일 데이터가 존재하면
-            console.log('이메일 중복')
-            res.json({result:false});
-        }else{
-            const hashPW = bcryptPassword(pw); //비밀번호 암호화
-            await User.create({email, name, phone, pw: hashPW})
-            res.json({result: true});
-        }
-        
-    }catch(error){
-        console.log('회원가입 error',error)
-        res.status(500).json({result:false})
+//회원가입
+exports.post_join = async (req, res) => {
+  console.log(req.body);
+  try {
+    const { email, name, phone, pw } = req.body;
+    console.log(req.body);
+    const existCheck = await User.findOne({ where: { email } });
+    if (existCheck) {
+      //이메일 데이터가 존재하면
+      console.log("이메일 중복");
+      res.json({ result: false });
+    } else {
+      const hashPW = bcryptPassword(pw); //비밀번호 암호화
+      await User.create({ email, name, phone, pw: hashPW });
+      res.json({ result: true });
     }
-}
+  } catch (error) {
+    console.log("회원가입 error", error);
+    res.status(500).json({ result: false });
+  }
+};
 
 //로그인
-exports.post_login = async (req, res)=>{
-    console.log('3');
-    try{
-        const {email, pw} = req.body;
-        console.log('4')
-        const result = await User.findOne({where: {email}})
-        if(!result){
-            console.log('사용자 존재안함')
-            res.json({result:false, message: '사용자가 존재하지 않습니다.'})
-        }else{
-            const compare = comparePassword(pw, result.pw);//비밀번호 암호화
-            if(compare){
-                console.log('로그인 성공')
-                req.session.isLogined = true; // 세션생성,
-                req.session.userId = result.id;
-                req.session.userName = result.name;
-                res.json({result:true})
-                // res.redirect('/profile')
-            }else{
-                console.log('비밀번호 불일치')
-                res.json({result: false, message: '비밀번호가 일치하지 않습니다.'})
-            }
-        }
-    }catch(error){
-        console.log('로그인 error',error)
+exports.post_login = async (req, res) => {
+  console.log("3");
+  try {
+    const { email, pw } = req.body;
+    console.log("4");
+    const result = await User.findOne({ where: { email } });
+    if (!result) {
+      console.log("사용자 존재안함");
+      res.json({ result: false, message: "사용자가 존재하지 않습니다." });
+    } else {
+      const compare = comparePassword(pw, result.pw); //비밀번호 암호화
+      if (compare) {
+        console.log("로그인 성공");
+        req.session.isLogined = true; // 세션생성,
+        req.session.userId = result.id;
+        req.session.userName = result.name;
+        res.json({ result: true, userid: req.session.userId });
+        // res.redirect('/profile')
+      } else {
+        console.log("비밀번호 불일치");
+        res.json({ result: false, message: "비밀번호가 일치하지 않습니다." });
+      }
     }
-}
-
-
+  } catch (error) {
+    console.log("로그인 error", error);
+  }
+};
 
 //프로필 사진
-exports.profileImg = (req, res)=>{
-    console.log(req.file); //userfile이 담김
-    res.send(req.file);
-}
-
-
-
-
+exports.profileImg = (req, res) => {
+  console.log(req.file); //userfile이 담김
+  res.send(req.file);
+};
 
 //////////////PATCH//////////////////////
-exports.edit_profile = async (req, res)=>{
-    const { id, email, name, phone } = req.body;
-    console.log(req.body)
-    const data = await User.update( {email, name, phone}, {where: {id}});
-    console.log('update', data);
-    res.json({result:true, data})
-}
-
-
+exports.edit_profile = async (req, res) => {
+  const { id, email, name, phone } = req.body;
+  console.log(req.body);
+  const data = await User.update({ email, name, phone }, { where: { id } });
+  console.log("update", data);
+  res.json({ result: true, data });
+};
 
 //암호화 함수
 
 const salt = 10;
-const bcryptPassword = (password) =>{
-    return bcrypt.hashSync( password, salt);
-  };
-  
-  //비교
-  const comparePassword = (password, dbPassword) => {
-    return bcrypt.compareSync(password, dbPassword); 
-  };
+const bcryptPassword = (password) => {
+  return bcrypt.hashSync(password, salt);
+};
 
+//비교
+const comparePassword = (password, dbPassword) => {
+  return bcrypt.compareSync(password, dbPassword);
+};
--- controller/SN_socket.js
+++ controller/SN_socket.js
@@ -1,2 +1,39 @@
 const { User, Sequelize } = require("../models");
 const Op = Sequelize.Op;
+
+//내 이름 저장
+let myID;
+//내 이메일 값
+let userEmail;
+
+exports.connection = (io, socket) => {
+  console.log("접속했습니다.~");
+  ////////////////변수명////////////////
+
+  ////////////////////////////////////////
+
+  ///////////////함수//////////////////
+
+  //myID에서 이메일 값 추출하는 함수
+  async function idExtract(id) {
+    const email = await User.findOne({
+      where: { id: id },
+    });
+    return email.email;
+  }
+
+  /////////////////////////////////////
+
+  //로그인이 성공하면 받는 이메일
+  socket.on("loginSuccess", async (e) => {
+    //id에서 이메일 값 찾아서 저장하는거
+    userEmail = await idExtract(e);
+    console.log("!!!!!!!!111111");
+    socket?.join(userEmail);
+  });
+  console.log("!!!!!!!!!!!!!!222222");
+  //접속할 때마다 계속 내 이메일로 된 방으로 들어감
+  console.log("접속한 내 방 이름 : ", userEmail);
+};
+
+//////////connection 끝////////////
