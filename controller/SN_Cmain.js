7b706a95b4ba7b61ae0840290108c0570d0f7260
SangNyungLee <lsy6747@gmail.com>
2023-09-07 04:41:40 +0900

    기능추가

--- controller/SN_Cmain.js
+++ controller/SN_Cmain.js
@@ -1,6 +1,18 @@
-const { User, Sequelize } = require("../models");
+const { User, Sequelize, Chat } = require("../models");
 const Op = Sequelize.Op;
 
 exports.chat = (req, res) => {
   res.render("SN_chat");
 };
+
+exports.preMessage = async(req,res)=>{
+  try {
+    let chatMessage = await Chat.findAll({
+      where : {roomNum : req.body.roomNum},
+    });
+    res.send({chatMessage});
+  } catch (error) {
+    console.log("에러==============", error)
+  }
+
+}
