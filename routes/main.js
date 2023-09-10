f35073e5b66e5749fe87f8c09280dd5d1702faa3
thbrthbr <tarzenezrat@gmail.com>
2023-09-11 01:07:29 +0900

    FEAT/ 노트매니저기능전반구현

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 const C_controller = require('../controller/CH_Cmain');
@@ -10,49 +10,45 @@ const path = require('path');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
 
-
 ///////Board page
-router.get("/board", C_controller.BoardMain);
+router.get('/board', C_controller.BoardMain);
 
 //////Board Detail page
-router.get("/board/detail", C_controller.BoardDetail);
+router.get('/board/detail', C_controller.BoardDetail);
 
 //////Board Write
-router.post("/board/write", C_controller.BoardWrite);
+router.post('/board/write', C_controller.BoardWrite);
 
 //////Board Search
-router.post("/board/search", C_controller.BoardSearch);
+router.post('/board/search', C_controller.BoardSearch);
 
 //////Board Delete
-router.delete("/board/delete", C_controller.BoardDelete);
+router.delete('/board/delete', C_controller.BoardDelete);
 
 //////Board Like
 
-
 router.post('/board/like', C_controller.BoardLike);
 
-
 //////comment Write
-router.post("/comment/write", C_controller.CommentWrite);
+router.post('/comment/write', C_controller.CommentWrite);
 
 //메인화면
 
-router.get("/", M_controller.main);
-router.post("/", M_controller.post_main);
+router.get('/', M_controller.main);
+router.post('/', M_controller.post_main);
 
 //회원가입 페이지
-router.get("/join", M_controller.join);
-router.post("/join", M_controller.post_join);
+router.get('/join', M_controller.join);
+router.post('/join', M_controller.post_join);
 
 //카카오 회원가입
 
 router.get('/auth/kakao/join', M_controller.kakao_get_join);
 router.get('/auth/kakao/join/callback', M_controller.kakao_join);
 
-
 //로그인 페이지
-router.get("/login", M_controller.login);
-router.post("/login", M_controller.post_login);
+router.get('/login', M_controller.login);
+router.post('/login', M_controller.post_login);
 
 //카카오 로그인
 
@@ -68,13 +64,12 @@ router.get('/auth/google/join/callback', M_controller.google_join);
 router.get('/auth/google', M_controller.google_get);
 router.get('/auth/google/login/callback', M_controller.google_callback);
 
-
 //multer 설정(보통 설정 파일을 따로 만들어서 exports 해서 씀.)
 //diskStorage: 파일 저장 관련 설정 객체
 const storage = multer.diskStorage({
   //저장될 경로지정
   destination: (req, file, cb) => {
-    cb(null, "public/uploads/");
+    cb(null, 'public/uploads/');
   },
   //파일 이름 설정
   filename: (req, file, cb) => {
@@ -100,10 +95,13 @@ const uploadS3 = multer({
     bucket: 'kdt9-thbr-test', // aws내에 존재하는 버킷 중에 지금 쓸거
     acl: 'public-read', // 파일접근권한 (public-read로 해야 업로드된 파일이 공개)
     metadata: function (req, file, cb) {
+      console.log(file);
       cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
-      cb(null, file.originalname);
+      let naming = file.originalname.split('.')[0];
+      let ext = file.originalname.split('.')[1];
+      cb(null, naming + Date.now().toString() + '.' + ext);
     },
   }),
 });
@@ -117,10 +115,10 @@ const limits = {
 const upload = multer({ storage, limits });
 
 //프로필
-router.get("/profile", M_controller.profile);
-router.patch("/profile", M_controller.edit_profile);
+router.get('/profile', M_controller.profile);
+router.patch('/profile', M_controller.edit_profile);
 //프로필사진
-router.post("/upload", upload.single("dynamic"), M_controller.profileImg);
+router.post('/upload', upload.single('dynamic'), M_controller.profileImg);
 
 //로그아웃
 router.get('/logout', M_controller.logout);
@@ -132,7 +130,7 @@ router.get('/bob', M_controller.bob);
 
 //////////////////상녕/////////////////
 
-router.get("/chat", S_controller.chat);
+router.get('/chat', S_controller.chat);
 
 //자리배치관련
 
@@ -158,16 +156,21 @@ router.post(
 //   T_controller.noteUpload,
 // );
 
+router.post('/noteManager/upload_folder', T_controller.noteUpload_folder);
+
+router.delete('/noteManager/erase_files', T_controller.erase_files);
+router.patch('/noteManager/patch_files', T_controller.patch_files);
+
 router.get('/test', T_controller.test);
 
 //채팅방 render페이지
-router.get("/chat", S_controller.chat);
+router.get('/chat', S_controller.chat);
 //채팅 불러오기
-router.post("/preMessage", S_controller.preMessage);
+router.post('/preMessage', S_controller.preMessage);
 //내가 참여한 채팅방 목록 불러오기
-router.post("/myChatList", S_controller.myChatList);
+router.post('/myChatList', S_controller.myChatList);
 //내 참여방 미리보기 목록 불러오기
-router.post("/myMessage", S_controller.myMessage);
+router.post('/myMessage', S_controller.myMessage);
 //가입되어 있는 사람 목록 불러오기
-router.post("/userList", S_controller.userList);
+router.post('/userList', S_controller.userList);
 module.exports = router;
