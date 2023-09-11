e7e1ca6ac473c7a050bfe41a57f591c477a91cb6
SangNyungLee <lsy6747@gmail.com>
2023-09-12 04:04:13 +0900

    프로필사진 수정, 알람 버그수정

--- controller/SN_Cmain.js
+++ controller/SN_Cmain.js
@@ -121,3 +121,27 @@ exports.practice = (req, res) => {
   console.log("온 값은?????????", req.files[0]);
   res.send(req.files[0].location);
 };
+
+//내 프로필 사진 불러오기
+exports.myprofile = async(req,res)=>{
+  const mypicture = await User.findOne({
+    where : {name : req.body.username}
+  })
+  //내 사진 경로 보내주기
+  if(mypicture.profileImgPath == null){
+    //사진 없을 때 디폴트 사진으로 들어가게
+    mypicture.profileImgPath = "public\\default\\logo.png";
+  }
+  res.send({picName : mypicture.profileImgPath});
+}
+//상대방 프로필 사진 불러오기
+exports.otherprofile = async (req,res)=>{
+  const otherpicture = await User.findOne({
+    where : {name : req.body.otherRoom}
+  })
+  if(otherpicture.profileImgPath == null){
+    //사진 없을 때 디폴트 사진으로 들어가게
+    otherpicture.profileImgPath = "public\\default\\logo.png";
+  }
+  res.send({otherpicName : otherpicture.profileImgPath})
+}
