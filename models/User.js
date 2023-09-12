35f93178c84c8542128c4a31229b0acdc274a133
Asong Ma <woss3223@gmail.com>
2023-09-12 21:28:23 +0900

    profile

--- models/User.js
+++ models/User.js
@@ -1,47 +1,51 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require("sequelize");
 
-const Model = (sequelize) =>{
-    return sequelize.define('user', {
-        id :{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        email: {
-            type: DataTypes.STRING(40),
-            // allowNull: false,
-        },
-        pw: {
-            type: DataTypes.STRING(255),
-            // allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING(128),
-            // allowNull: false,
-        },
-        phone: {
-            type: DataTypes.INTEGER(11),
-            allowNull: true,
-        },
-        userType:{
-            type: DataTypes.STRING(11),
-            allowNull: true,
-        },
-        snsId: {
-            type: DataTypes.STRING(40),
-            allowNull: true,
-        },
-        profileImgPath: {
-            type: DataTypes.STRING(255),
-            allowNull: true,
-        }
+const Model = (sequelize) => {
+  return sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      email: {
+        type: DataTypes.STRING(40),
+        // allowNull: false,
+      },
+      pw: {
+        type: DataTypes.STRING(255),
+        // allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING(128),
+        // allowNull: false,
+      },
+      phone: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+      },
+      userType: {
+        type: DataTypes.STRING(11),
+        allowNull: true,
+      },
+      snsId: {
+        type: DataTypes.STRING(40),
+        allowNull: true,
+      },
+      profileImgPath: {
+        type: DataTypes.STRING(255),
+        defaultValue: "/public/default/defaultProfile.png",
+        allowNull: true,
+      },
     },
     {
-        tableName: 'user', //테이블 이름 설정
-        freezeTableName: true, //true로 설정하면 이름을 복수로 설정하지 않음
-        timestamps: true, // createAt & updateAt을 활성화
-    })
-}
+      tableName: "user", //테이블 이름 설정
+      freezeTableName: true, //true로 설정하면 이름을 복수로 설정하지 않음
+      timestamps: true, // createAt & updateAt을 활성화
+    }
+  );
+};
 
-module.exports = Model
+module.exports = Model;
